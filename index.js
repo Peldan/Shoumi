71582f294eb3357963aeb6f59e0c6fce6e24b778
Peldan <peldans@gmail.com>
2018-09-04 22:58:35 +0200

    added support for the application to be minimized to system tray

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu, Tray} = require('electron');
 const {ipcMain} = require ('electron');
 let window;
 let template = 'index.html';
 let fileName;
+let tray = null;
 
 function createWindow(){
     window = new BrowserWindow({width: 1200, height: 900});
@@ -12,7 +13,8 @@ function createWindow(){
             window.webContents.send('image-msg', fileName);
         })
     }
-    window.on('closed', () => {
+    window.on('closed', (event) => {
+        event.preventDefault();
         window = null;
     });
 
@@ -26,6 +28,15 @@ function setupNewWindow(name){
 
 app.on('ready', () => {
     createWindow();
+    tray = new Tray('./resources/trayicon.png');
+    const contextMenu = Menu.buildFromTemplate([
+        {label: 'Item1', type: 'radio'},
+        {label: 'Item2', type: 'radio'},
+        {label: 'Item3', type: 'radio', checked: true},
+        {label: 'Item4', type: 'radio'}
+    ])
+    tray.setToolTip("Shoumi - Image viewer");
+    tray.setContextMenu(contextMenu);
     ipcMain.on('asynchronous-message', (event, arg, fileName) => {
         if (arg === 'window-requested') {
             setupNewWindow(fileName);
