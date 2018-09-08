1a395cbe32a4799e869e0ce9dc5aad3f2902405a
Peldan <peldans@gmail.com>
2018-09-08 16:13:28 +0200

    minimize to tray now works in non-dev mode

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow, Menu, Tray } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const isDev = require('electron-is-dev');
+const log = require('electron-log');
+log.transports.file.level = "info";
+log.info('Hej!');
 let window;
 let template = 'index.html';
 let fileName;
@@ -10,9 +13,11 @@ let isQuitting = false;
 autoUpdater.logger = require('electron-log');
 autoUpdater.logger.transports.file.level = 'info';
 
+
 function createWindow(){
     window = new BrowserWindow({width: 880, height: 390});
     window.loadFile(template);
+    window.webContents.openDevTools();
     if(typeof fileName !== "undefined"){
         window.webContents.on('did-finish-load', () => {
             window.webContents.send('image-msg', fileName);
@@ -24,16 +29,21 @@ function createWindow(){
     });
 
     window.on('close', (event) => {
-        if(!isQuitting) {
-            event.preventDefault();
-            window.hide();
-        }
+        window = null;
+        app.quit();
     });
 }
 
 app.on('ready', () => {
     createWindow();
-    tray = new Tray('./build/trayicon.png');
+    if(isDev){
+        tray = new Tray('./build/icon.ico');
+    } else {
+        log.info("Dirname: " + __dirname + " resources: " + process.resourcesPath);
+        console.log(__dirname);
+        tray = new Tray(process.resourcesPath + '\\icon.ico');
+    }
+
     const contextMenu = Menu.buildFromTemplate([
         {label: 'Show', click: () => {
             window.show();
@@ -45,8 +55,9 @@ app.on('ready', () => {
     ])
     tray.setToolTip("Shoumi - Image viewer");
     tray.setContextMenu(contextMenu);
-    autoUpdater.checkForUpdates();
-
+    if(!isDev) {
+        autoUpdater.checkForUpdates();
+    }
 });
 
 autoUpdater.on('checking-for-update', () => {
@@ -74,8 +85,7 @@ autoUpdater.on('update-downloaded', (info) => {
 });
 
 autoUpdater.on('error', (error) => {
-    alert(error);
-    console.error(error);
+    log.info("Update error:" + error);
 });
 
 
